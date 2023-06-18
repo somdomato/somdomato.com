ee0c1c2d06a19b7b11f973524f96528ed5b66e42
Lucas Saliés Brum <97509+sistematico@users.noreply.github.com>
2023-06-18 15:23:02 -0400

    Update automático

---
+++ components/Pagination.vue
@@ -0,0 +1,78 @@
+<script setup>
+const currentPage = ref(1)
+const pageSize = ref(10)
+
+const emit = defineEmits(['page-changed'])
+const props = defineProps({ totalItems: { type: Number, default: 0 } })
+
+const totalPages = computed(() => Math.ceil(props.totalItems / pageSize.value))
+
+const visiblePageNumbers = computed(() => {
+  const maxVisiblePages = 5;
+  const pageNumbers = [];
+  let startPage = Math.max(currentPage.value - Math.floor(maxVisiblePages / 2), 1)
+  let endPage = Math.min(startPage + maxVisiblePages - 1, totalPages.value)
+
+  if (endPage - startPage < maxVisiblePages - 1) {
+    startPage = Math.max(endPage - maxVisiblePages + 1, 1)
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    pageNumbers.push(i)
+  }
+
+  return pageNumbers
+})
+
+function goToPage(pageNumber) {
+  if (pageNumber >= 1 && pageNumber <= totalPages.value) {
+    currentPage.value = pageNumber;
+    emit('page-changed', pageNumber);
+  }
+}
+</script>
+<template>
+  <!-- <div> -->
+    <!-- <ul>
+      <li v-if="currentPage > 1">
+        <button @click="goToPage(1)"><<</button>
+        <button @click="goToPage(currentPage - 1)"><</button>
+      </li>
+      <li v-for="pageNumber in visiblePageNumbers" :key="pageNumber">
+        <button @click="goToPage(pageNumber)" :class="{ active: pageNumber === currentPage }">{{ pageNumber }}</button>
+      </li>
+      <li v-if="currentPage < totalPages">
+        <button @click="goToPage(currentPage + 1)">></button>
+        <button @click="goToPage(totalPages)">>></button>
+      </li>
+    </ul>
+    <p>Total de registros: {{ totalItems }}</p> -->
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+
+        <li class="page-item" :class="{ 'disabled': currentPage === 1 }">
+          <a class="page-link shadow-none" href="#" aria-label="Previous" @click.prevent="goToPage(currentPage - 1)">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+
+        <li class="page-item" v-for="pageNumber in visiblePageNumbers" :key="pageNumber" :class="{ 'disabled': pageNumber === currentPage }">
+          <a class="page-link shadow-none" href="#" @click.prevent="goToPage(pageNumber)">{{ pageNumber }}</a>
+        </li>
+
+        <li class="page-item" :class="{ 'disabled': currentPage === totalPages }">
+          <a class="page-link shadow-none" href="#" aria-label="Next" @click.prevent="goToPage(currentPage + 1)">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+
+        <li class="page-item">
+          <a class="page-link disabled" href="#">
+            Total de registros: {{ totalItems }}            
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+  <!-- </div> -->
+</template>
--- components/Requests.vue
+++ components/Requests.vue
@@ -1,72 +1,82 @@
 <script setup>
 const config = useRuntimeConfig()
 const { $toast } = useNuxtApp()
-const searchResults = ref([])
-const searchTotal = ref(0)
+
 const search = ref('')
+const currentPage = ref(1);
+const pageSize = ref(10);
+const totalSongs = ref(0);
+const totalPages = ref(0);
+const songsResult = ref([]);
 
 async function doRequest(id, evt) {
   console.log(evt)
-  
+
   const { data } = await useFetch(`${config.public.apiBase}/pedido/${id}`)
   const { status, message } = toRaw(data.value)
 
   switch (status) {
     case 'success':
-      $toast.success(message)      
-      break;  
+      $toast.success(message)
+      break;
     case 'warning':
-      $toast.warning(message)      
-      break;  
+      $toast.warning(message)
+      break;
     case 'info':
-      $toast.info(message)      
-      break;  
+      $toast.info(message)
+      break;
     case 'error':
-      $toast.error(message)      
-      break;  
+      $toast.error(message)
+      break;
     default:
       $toast.error(message)
       break;
   }
-
-  // searchResults.value = []
 }
 
-async function doSearch() {
-  if (search.value !== '' && search.value.length > 3) {
-    const { data } = await useFetch(`${config.public.apiBase}/musica/?q=${search.value}`)
-    const { songs } = toRaw(data.value)
-    searchResults.value = songs.data
-    searchTotal.value = songs.total
+async function fetchSongs(pageNumber) {
+  currentPage.value = pageNumber || 1;
+
+  if (search.value !== '' && search.value.length > 1) {
+    const response = await useFetch(`${config.public.apiBase}/musica/?q=${search.value}&p=${currentPage.value}&t=${pageSize.value}`)
+    const { songs, total } = toRaw(response.data.value)
+    
+    songsResult.value = songs
+    totalSongs.value = total
+
+    totalPages.value = Math.ceil(totalSongs.value / pageSize.value)
   }
 }
+
+watch(currentPage, fetchSongs)
 </script>
 <template>
   <div class="col-12 order-4 mb-3">
     <div class="bg-dark rounded-2 p-2 h-100">
       <h2>Pedidos</h2>
-      <form class="d-flex" role="search" @submit.prevent="doSearch">
+      <form class="d-flex" role="search" @submit.prevent="fetchSongs()">
         <div class="input-group input-group mb-3">
           <input ref="inputsearch" v-model="search" type="text" class="form-control bg-dark text-white"
             placeholder="Música ou Artista" aria-label="Música ou Artista" aria-describedby="button-addon"
             style="font-size: 16px;" />
-          <button class="btn btn-danger" type="reset" id="button-addon">Limpar</button>
+          <button class="btn btn-danger" type="reset" id="button-addon" @click="songsResult = []">Limpar</button>
           <button class="btn btn-primary" type="submit" id="button-addon">Pesquisar</button>
         </div>
       </form>
       <table class="table table-borderless table-dark">
         <tbody>
-          <tr v-for="(item, index) in searchResults">
+          <tr v-for="(item, index) in songsResult">
             <th scope="row">{{ index + 1 }}</th>
             <td>{{ item.artist }}</td>
             <td>{{ item.title }}</td>
             <td>
-              <button type="button" class="btn btn-success btn-sm" @click="doRequest(item.id, $event)">Pedir</button>
+              <button type="submit" class="btn btn-success btn-sm" @click="doRequest(item.id, $event)">Pedir</button>
             </td>
           </tr>
         </tbody>
       </table>
-      <p v-if="searchResults.length > 1">Total: {{ searchTotal }}</p>
+
+      <Pagination :totalItems="totalSongs" @page-changed="fetchSongs" v-if="songsResult.length > 1" />
     </div>
   </div>
 </template>
